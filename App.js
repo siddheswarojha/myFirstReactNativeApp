aa7cc3562a110732d46bed9b8504e7c572fc7d95
siddheswarojha <siddheswarojha23@gmail.com>
2023-04-17 23:59:03 +0530

    initializing new files

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView,
+  TouchableHighlight,TouchableOpacity,
+Button } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+    style={{
+      backgroundColor : "#fff",
+      flex : 1
+    }}
+    >
+    <View
+    style={{
+      backgroundColor:"orange",
+      flex:0.5
+    }}/>
+     <View
+    style={styles.container}>
+      <Image
+      alignSelf='center'
+
+      style={styles.imageStyle}
+      source={require("./assets/images.png")}/>
+    </View>
+     <View
+    style={{
+      backgroundColor:"green",
+      flex:0.5
+    }}/>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 0.5,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:20
   },
+  textStyle:{
+    color: "red",
+    backgroundColor: "green",
+    fontWeight: "bold"
+  },
+  btnStyle:{
+    color:"orange",
+    textDecorationColor:"blue",
+    fontWeight:"italic",
+    justifyContent: 'center',
+    alignItems:'left'
+  },
+  imageStyle:{
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
